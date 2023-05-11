0b2773799b9dc953c3bcf1cf4ff54a2ffa14cffc
Siddharth <officialsiddharthbisht@gmail.com>
2023-05-11 10:44:57 +0530

    feat: first commit

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {SafeAreaView, ScrollView, View, Text,StyleSheet} from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+function App(){
+  return(
+    <SafeAreaView>
+      <ScrollView>
+        <View>
+          <Text>
+            Hello Siddharth
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  )
+
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ components/FlatCard.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function FlatCard() {
+  return (
+    <View>
+      <Text>FlatCard</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
